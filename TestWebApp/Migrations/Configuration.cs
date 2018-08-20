9beb4bf8e1e10614f1deabf7221d4f7a38842a2a
JoasE <32096708+JoasE@users.noreply.github.com>
2018-08-20 21:57:24 +0200

    Installed EF6 and enabled migrations

---
+++ TestWebApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TestWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TestWebApp.Models.TestWebAppContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TestWebApp.Models.TestWebAppContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
